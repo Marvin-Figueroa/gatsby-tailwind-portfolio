703b344994e7f0fe8ee85cbbe0f9bb3fd3911467
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2023-01-03 22:42:32 -0600

    Add required plugins configurations

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,14 +1,35 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
+    title: `Marvin Figueroa | Portfolio`,
     siteUrl: `https://www.yourdomain.tld`,
+    description: 'Frontend Web Developer Portfolio',
+    copyright: 'Marvin Figueroa - Frontend Dev - 2023',
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: [],
-}
 
-export default config
+  graphqlTypegen: true,
+  plugins: [
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-transformer-json',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `data`,
+        path: `./data`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+  ],
+};
+
+export default config;
