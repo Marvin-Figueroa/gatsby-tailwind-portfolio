802036d9be5fe6ce26ac52d7c7f37933e27e8421
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2023-01-03 22:39:37 -0600

    Add Layout wrapper and banner to IndexPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,29 @@
 import * as React from 'react';
-import type { HeadFC, PageProps } from 'gatsby';
+import { HeadFC, PageProps } from 'gatsby';
+import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <main>
-      <h2>Marvin Figueroa - Frontend Developer</h2>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-        repellendus et incidunt dolore? Ex, magni officiis! Ducimus, at?
-        Explicabo consequatur ea ullam nisi dolorem optio expedita eveniet,
-        magni, veritatis, possimus nulla ipsum architecto magnam esse? Iusto
-        asperiores vitae quae repudiandae. Ipsum sed ea asperiores neque nulla
-        nemo pariatur dolorum quasi.
-      </p>
-    </main>
+    <Layout className='flex justify-between items-center'>
+      <section>
+        <h2>Marvin Figueroa - Frontend Developer</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+          repellendus et incidunt dolore? Ex, magni officiis! Ducimus, at?
+          Explicabo consequatur ea ullam nisi dolorem optio expedita eveniet,
+          magni, veritatis, possimus nulla ipsum architecto magnam esse? Iusto
+          asperiores vitae quae repudiandae. Ipsum sed ea asperiores neque nulla
+          nemo pariatur dolorum quasi.
+        </p>
+      </section>
+      <section>
+        <StaticImage
+          src='../images/undraw-programming.svg'
+          alt='A developer banner'
+        />
+      </section>
+    </Layout>
   );
 };
 
