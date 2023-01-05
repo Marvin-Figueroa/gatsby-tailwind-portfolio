96587dad72968ec46ee52772289d2802a5e0b5cf
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2023-01-04 23:30:57 -0600

    Add 404 image and classes for styling

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,29 @@
 import * as React from 'react';
 import { Link, HeadFC, PageProps } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
-    <main>
-      <h1>Page Not Found</h1>
-      <p>
+    <main className='container-xl flex flex-col justify-center  items-center gap-8 text-center'>
+      <h1 className='text-4xl mt-6 font-bold text-red-500'>Page Not Found</h1>
+      <StaticImage
+        src='../images/undraw-page-not-found.svg'
+        alt='A developer banner'
+        className='max-w-lg'
+      />
+      <p className='text-2xl'>
         Sorry 😔, we couldn’t find what you were looking for.
-        <Link to='/'>Go home</Link>.
       </p>
+      <Link
+        to='/'
+        className='rounded-xl py-2 px-4 bg-green-500 hover:bg-green-600 text-white font-semibold'>
+        Go Home
+      </Link>
+      .
     </main>
   );
 };
 
 export default NotFoundPage;
 
-export const Head: HeadFC = () => <title>Not found</title>;
+export const Head: HeadFC = () => <title>Not Found</title>;
