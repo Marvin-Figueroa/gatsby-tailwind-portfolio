772c55e2c25fc75e946febb6bfe0e131c73f5719
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2023-01-05 00:03:39 -0600

    Add missing title to project's page

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,4 +1,4 @@
-import { graphql, Link } from 'gatsby';
+import { graphql, HeadFC, Link } from 'gatsby';
 import React, { useState } from 'react';
 import Layout from '../../components/layout';
 import { GatsbyImage } from 'gatsby-plugin-image';
@@ -119,3 +119,5 @@ export const query = graphql`
     }
   }
 `;
+
+export const Head: HeadFC = () => <title>Projects</title>;
